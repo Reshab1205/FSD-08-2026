5f1ebcfbc23b3b6fcbdba214cfe86ce89a7ebb1c
Reshab Kumar <rkmodi12051998@gmail.com>
2026-01-27 17:05:41 +0530

    schema validations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,41 @@
 const express = require("express");
+const userModel = require("../models/userModel");
 
 const register = async (req, res) => {
   try {
     const inputData = req.body;
-    console.log(inputData);
+    console.log(Object.keys(inputData).length);
+    if (Object.keys(inputData).length === 0) {
+      return res.json({
+        status_code: 404,
+        message: "Provide Proper Data for Registration",
+      });
+    }
+    const checkEmail = await userModel.findOne({ email:inputData.email})
+    const checkMobile = await userModel.findOne({ mobile_number:inputData.mobile_number})
+    const checkAadhar = await userModel.findOne({ aadhar_number:inputData.aadhar_number})
+
+
+    if(checkEmail || checkMobile || checkAadhar) {
+      return res.json({
+      status_code: 404,
+      message: "User Exists Already",
+    });
+    }
+
+    // console.log(inputData);
+    const storeDb = await userModel.create(inputData)
     return res.json({
       status_code: 200,
       message: "Registered Successfully",
-      data: inputData,
+      data: storeDb,
     });
   } catch (err) {
-    return res.json({ 
-        status_code: 404, 
-        message: "Registered Unsuccessfull" });
+    console.log(err);
+    return res.json({
+      status_code: 404,
+      message: "Registered Unsuccessfull",
+    });
   }
 };
 
---
+++ models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    full_name: {type: String},
+    email: {type:String, unique:true, required:true},
+    mobile_number:{type:String, unique:true, required:true},
+    password:{type:String},
+    aadhar_number:{type:Number, unique:true},
+    aadhar_kyc_status:{type:Boolean , default:false},
+    address:{
+        address_line_1 :{type:String},
+        address_line_2:{type:String},
+        area:{type:String},
+        city:{type:String},
+        state:{type:String},
+        pincode:{type:Number},
+        nation:{type:String, default:"Bharat"},
+    },
+    isActive:{type:String, enum:["Active", "InActive", "Banned"]}
+})
+
+module.exports = mongoose.model("user", userSchema)
